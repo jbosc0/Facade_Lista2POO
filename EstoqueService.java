319b08abdc9a8351a9d44005b9e33311d506c3f1
João Bosco <107318479+jbosc0@users.noreply.github.com>
2025-12-02 20:39:50 -0300

    feat: adicionar EstoqueService para validar disponibilidade dos produtos

---
+++ EstoqueService.java
@@ -0,0 +1,7 @@
+public class EstoqueService {
+
+    public boolean verificarDisponibilidade(Pedido pedido) {
+        System.out.println("[Estoque] Verificando disponibilidade dos produtos...");
+        return true; // simulação: sempre disponível
+    }
+}
